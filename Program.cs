ad179cfb5379e3c90670a1f455f68ff4212ad63a
MohieSrour <srormohi@gmail.com>
2026-09-08 23:06:28 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+/*
+/*variables is a storage location    يعنى مكان تخزين وله نوع وياخذ قيمة 
+Data Type ...why data type 
+            data type help compiler to determine that:
+                    1)size of varible
+                    2)the operators of this variable
+                    3)the values variables can equal
+ */
+//Declaration variable
+//int age =10;    
+/*int age;    // دون اعطاءه قيمة ageبان هناك متغير انا انشاته اسمه compiler تعريف ال
+age = 10;   //اعطاء قيمة للمتغير 
+Console.WriteLine(age);
+age = 38;   // تغيير قيمة المتغير 
+Console.WriteLine(age);
+/*
+                            varibles Naming Rules:
+     1-unique name                              يكون اسم فريد وغير مكرر
+     2-case sensetive حساسية اللغة للحروف */
+/*int numm;                 // متغير 
+int Numm;        // متغير اخر 
+     //3-cannot start with numm      ولكن يمكن اضافة الرقم فى النهاية او المنتصف
+//int 2age;         It is wrong 
+     /*4-reseved keywords  (public ,class--->كلمات محجوزة فى المترجم )
+     5-No spaces or special charactres
+ */
+                     // constant and variables 
+//const int Age = 14;          //انشاء الثابت 
+//لا يمكن التغيير فى الثابت او تغيير قيمته مثل المتغيرات 
+//Age = 12;            ظهر خطا 
+
---
+++ operators.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("programm is started");
+        Arthimetic.Print();
+        Assignment AS = new Assignment();
+        AS.print_As();
+        Compresion co = new Compresion();
+        co.print_comp();
+        Logical lo  = new Logical();
+        lo.print_lo();
+
+
+    }
+}
+/*                                operators 
+               it is code use to tell compiler to do specific operate 
+                    like:  (+)------>that tell compiler to add variables
+note the diffrenate:
+ operator----->(+),(-)..
+ operaned ----->the variable or value like (x,y ) in (x+y)
+ statment ------->(int result =x+y)
+ expresion ------->(x+y)
+ */
+public  class Arthimetic
+{
+   
+    public static void Print()
+    {
+        int x = 10;
+        int y = 20;
+        double x1 = 10;
+        double y1 = 20;
+        Console.WriteLine(x + y);
+        Console.WriteLine(x1 + y1);
+        Console.WriteLine(x - y);
+        Console.WriteLine(x / y);
+        Console.WriteLine(x * y);
+        Console.WriteLine(x % y);
+    }
+
+}  
+public class Assignment
+{
+    //assignment operator (=,+=,-=,*=,/=,%=)
+    int Ass = 20;
+    public void print_As()
+    {
+        Ass = 30;  // Assignment Ass---->30
+        Ass += 3;  //Ass=Ass+3
+        Ass = Ass + 3;
+        Console.WriteLine(Ass);
+        Ass -=3;  //Ass=Ass-3
+        Ass = Ass -3;
+        Console.WriteLine(Ass);
+        Ass *= 3; //Ass=Ass*3
+        Console.WriteLine(Ass);
+    }
+}
+// compersion use to compare between two or more values 
+class Compresion
+{
+    int comp = 12;
+    public void print_comp()
+    {
+        Console.WriteLine(comp == 5);
+        Console.WriteLine(comp != 5);
+        Console.WriteLine(comp > 5);
+        Console.WriteLine(comp < 5);
+        Console.WriteLine(comp <= 5);
+        Console.WriteLine(comp >= 5);
+    }
+}
+// compersion use one condition but when two or more conditions we use Logical
+class Logical
+{
+    int comp1 = 12;
+
+    bool comp2 = true;
+    public void print_lo()
+    {
+        Console.WriteLine(comp1 == 12 && comp2==true);
+        Console.WriteLine(comp1 == 12 || comp2 == true);
+        Console.WriteLine(!comp2 );
+
+    }
+}
